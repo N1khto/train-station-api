f0483ff994f11e2a6e40cff156c876aa647c7442
Volodymyr Bohomol <8773927@gmail.com>
2023-09-06 16:26:24 +0300

    - created view, two serializer and url router for Route model

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -1,9 +1,27 @@
 from rest_framework import serializers
 
-from train_station.models import Station
+from train_station.models import Station, Route
 
 
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
         fields = ("id", "name", "latitude", "longitude",)
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    source = serializers.CharField(source="source.name", read_only=True)
+    destination = serializers.CharField(source="destination.name", read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance",)
+
+
+class RouteDetailSerializer(RouteSerializer):
+    source = StationSerializer(many=False, read_only=True)
+    destination = StationSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ("id", "source", "destination", "distance",)
--- train_station/views.py
+++ train_station/views.py
@@ -1,14 +1,15 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
-from train_station.models import Station
+from train_station.models import Station, Route
 from train_station.permissions import IsAdminOrIfAuthenticatedReadOnly
-from train_station.serializers import StationSerializer
+from train_station.serializers import StationSerializer, RouteSerializer, RouteDetailSerializer
 
 
 class StationViewSet(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
     GenericViewSet,
 ):
     queryset = Station.objects.all()
@@ -16,3 +17,17 @@ class StationViewSet(
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
