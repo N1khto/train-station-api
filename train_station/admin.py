d7d6369f844245e35226115d9ff497b3fdc8ec99
Volodymyr Bohomol <8773927@gmail.com>
2023-09-06 13:37:41 +0300

    - registered train_station app models in admin panel - unregister `Group`

--- train_station/admin.py
+++ train_station/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .models import Train, TrainType, Station, Route, Crew, Journey, Order, Ticket
+
+
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Station)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(Journey)
+admin.site.register(Order)
+admin.site.register(Ticket)
+
+admin.site.unregister(Group)
