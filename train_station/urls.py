1803d7d4093aff98defe2e4003d26f1040ec7dc8
Volodymyr Bohomol <8773927@gmail.com>
2023-09-06 15:58:36 +0300

    - added permissions.py and serializers.py to train_station app - created view, serializer and url router for Station model

--- train_station/urls.py
+++ train_station/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from train_station.views import StationViewSet
 
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("stations", StationViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "train_station"
--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from train_station.models import Station
+from train_station.permissions import IsAdminOrIfAuthenticatedReadOnly
+from train_station.serializers import StationSerializer
+
+
+class StationViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
